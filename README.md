9793a02c8e2627debfc1486e47096d30277d3bd9
Hiteshtechark <htarasariya.techark@gmail.com>
2023-07-21 10:46:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Faust Offset Pagination Example
---
+++ pages/blog/page/[pageNumber].js
@@ -0,0 +1,97 @@
+import { gql, useQuery } from "@apollo/client";
+import { getNextStaticProps } from "@faustwp/core";
+import Link from "next/link";
+
+export const POSTS_PER_PAGE = 2;
+
+function getOffsetFromPageNumber(pageNumber) {
+  return pageNumber === 1 ? 0 : (pageNumber - 1) * POSTS_PER_PAGE;
+}
+
+export default function Page(props) {
+ 
+  const { data } = useQuery(Page.query, {
+    variables: {
+      postsPerPage: POSTS_PER_PAGE,
+      offset: getOffsetFromPageNumber(props.pageNumber),
+    },
+  });
+
+  const totalPosts = data.posts.pageInfo.offsetPagination.total;
+  const numPages = Math.floor(totalPosts / POSTS_PER_PAGE);
+
+  return (
+    <>
+      <p>Total Posts: {data.posts.pageInfo.offsetPagination.total}</p>
+
+      <h2>Posts</h2>
+      <ul>
+        {data.posts.nodes.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
+      <h2>Pagination</h2>
+      <ul>
+        {[...Array(numPages)].map((x, i) => {
+          const pageNum = i + 1;
+
+          return (
+            <li key={i}>
+              <Link key={i} href={`/blog/page/${pageNum}`}>
+                page {pageNum}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+Page.query = gql`
+  query OffsetPaginatedQuery($postsPerPage: Int, $offset: Int) {
+    posts(
+      where: { offsetPagination: { size: $postsPerPage, offset: $offset } }
+    ) {
+      pageInfo {
+        offsetPagination {
+          hasMore
+          hasPrevious
+          total
+        }
+      }
+      nodes {
+        id
+        title
+      }
+    }
+  }
+`;
+
+
+Page.variables = ({ params }) => {
+  const { pageNumber = 1 } = params ?? {};
+
+  return {
+    postsPerPage: POSTS_PER_PAGE,
+    offset: getOffsetFromPageNumber(pageNumber),
+  };
+};
+
+export function getStaticProps(ctx) {
+  
+  return getNextStaticProps(ctx, {
+    Page,
+    props: {
+      pageNumber: ctx?.params?.pageNumber ?? 1,
+    },
+  });
+}
+
+export function getStaticPaths(ctx) {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
